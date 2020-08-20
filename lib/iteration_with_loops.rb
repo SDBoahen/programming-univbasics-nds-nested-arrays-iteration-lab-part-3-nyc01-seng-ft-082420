4b681b5dc5dcd9fd2faa362d889bb216c93eacc8
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-20 20:31:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,38 @@
-def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
-  # Combine all Strings present in the AoA into a single value and return it
+# src will be an Array of Arrays of Strings and Integers
+
+# Combine all Strings present in the AoA into a single value and return it
+
+
+def 
+  
+  join_nested_strings(src)
+  
+
+  puts "****#### TESTS ####****"
+  # `
+  test1 = 1
+  test2 = 2.2
+  test3 = "stringie-tingie"
+  
 end
+
+
+
+
+# mixed_data = 
+# [
+#   ["The", 4, "quick"],
+#   [-1, "brown", "fox", 30],
+#   ["studied", 101, 233, "Ruby"]
+# ]
+ 
+# join_nested_strings(mixed_data)
+# We should expect the following returned:
+
+# "The quick brown fox studied Ruby"
+
+
+#X |  ***  .to_s          !!
+# L> Reread instructions -- We Are Not Being Asked For All The Elements in 1 (String)
+# ***  NEED (.class)
+  
